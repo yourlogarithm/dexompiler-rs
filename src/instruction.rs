392f22e986f516373db5c5d0fae4e926022cb43e
Vlad-Sebastian Cretu <vcretu@bitdefender.com>
2023-10-14 14:10:04 +0300

    no more vector copying

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,5 @@
+use std::os::unix::raw;
+
 use crate::opcode::Opcode;
 
 #[derive(Debug)]
@@ -14,7 +16,10 @@ enum Type {
 }
 
 #[derive(Debug)]
-enum Format {
+enum Argument {
+    // May hold an 8-bit register number or 2 4-bit register numbers
+    PackedRegister(u8),
+    WideRegister(u16),
     ImmediateSignedByte(i8),
     ConstantPoolIndex16(Type, u16),
     ConstantPoolIndex32(Type, u32),
@@ -33,7 +38,7 @@ pub struct Instruction {
     /// The opcode of the instruction
     pub opcode: Opcode,
     /// Non-register arguments to the instruction
-    arguments: Vec<Format>,
+    arguments: Vec<Argument>,
     /// The length of the instruction
     length: u8 ,
     /// The offset of the instruction
@@ -41,57 +46,94 @@ pub struct Instruction {
 }
 
 
-macro_rules! extract_from_le {
-    ($type:ty, $arr:expr, $idx:expr) => {{
-        let size = std::mem::size_of::<$type>();
-        let mut byte_slice: Vec<u8> = vec![];
-        for i in 0..size {
-            byte_slice.push($arr[$idx + i]);
-        }
-        <$type>::from_le_bytes(byte_slice.try_into().unwrap())
-    }};
+macro_rules! concat_words {
+    // Case for concatenating 4 words into u64
+    ($a:expr, $b:expr, $c:expr, $d:expr) => {
+        ((($a as u64) << 48) |
+         (($b as u64) << 32) |
+         (($c as u64) << 16) |
+          ($d as u64))
+    };
+    // Case for concatenating 2 words into u32
+    ($a:expr, $b:expr) => {
+        (($a as u32) << 16) | ($b as u32)
+    };
 }
 
+macro_rules! split_word {
+    ($word:expr) => {
+        (($word & 0xff) as _, ($word >> 8) as _)
+    };
+}
 
 impl Instruction {
     pub fn length(&self) -> u8 {
         self.length
     }
 
-    pub fn try_from_bytes(bytecode: &[u8], offset: usize) -> Result<Option<Self>, &'static str> {
-        let bytes = &bytecode[offset..];
-        if let Ok(opcode) = Opcode::try_from(bytes[0]) {
-            if let Ok::<(Vec<Format>, u8), &'static str>((arguments, length)) = match bytes[0] {
+    pub fn try_from_raw_bytecode(raw_bytecode: &[u16], offset: usize) -> Result<Option<Self>, &'static str> {
+        let raw_bytecode = &raw_bytecode[offset..];
+        let (opcode_byte, immediate_args) = split_word!(raw_bytecode[0]);
+        if let Ok(opcode) = Opcode::try_from(opcode_byte) {
+            if let Ok::<(Vec<Argument>, u8), &'static str>((arguments, length)) = match opcode_byte {
                 0x0 => {
-                    if bytes.len() > 1 && (1..=3).contains(&bytes[1]) {
+                    if (1..=3).contains(&immediate_args) {
                         return Ok(None);
                     } else {
-                        Ok((vec![], 2))
+                        Ok((vec![], 1))
                     }
                 },
-                0x1 | 0x4 | 0x7 | 0xa..=0x11 | 0x1d | 0x1e | 0x21 | 0x27 | 0x7b..=0x8f | 0xb0..=0xcf => Ok((vec![], 2)),
-                0x2 | 0x5 | 0x8 | 0x2d..=0x31 | 0x44..=0x51 | 0x90..=0xaf => Ok((vec![], 4)),
-                0x3 | 0x6 | 0x9 => Ok((vec![], 6)),
-                0xd8..=0xe2 => Ok((vec![Format::ImmediateSignedByte(bytes[3] as i8)], 4)),
-                0x1a => Ok((vec![Format::ConstantPoolIndex16(Type::String, extract_from_le!(u16, bytes, 2))], 4)),
-                0x1c | 0x1f | 0x20 | 0x22 | 0x23 => Ok((vec![Format::ConstantPoolIndex16(Type::Type, extract_from_le!(u16, bytes, 2))], 4)),
-                0x52..=0x5f => Ok((vec![Format::ConstantPoolIndex16(Type::InstanceField, extract_from_le!(u16, bytes, 2))], 4)),
-                0x60..=0x6d => Ok((vec![Format::ConstantPoolIndex16(Type::StaticField, extract_from_le!(u16, bytes, 2))], 4)),
-                0xfe => Ok((vec![Format::ConstantPoolIndex16(Type::MethodHandle, extract_from_le!(u16, bytes, 2))], 4)),
-                0xff => Ok((vec![Format::ConstantPoolIndex16(Type::MethodPrototype, extract_from_le!(u16, bytes, 2))], 4)),
-                0x1b => Ok((vec![Format::ConstantPoolIndex32(Type::String, extract_from_le!(u32, bytes, 2))], 6)),
-                0x24 | 0x25 => Ok((vec![Format::ConstantPoolIndex32(Type::Type, extract_from_le!(u32, bytes, 2))], 6)),
-                0x6e..=0x72 | 0x74..=0x78 => Ok((vec![Format::ConstantPoolIndex32(Type::Method, extract_from_le!(u32, bytes, 2))], 6)),
-                0xfc | 0xfd => Ok((vec![Format::ConstantPoolIndex32(Type::CallSite, extract_from_le!(u32, bytes, 2))], 6)),
-                0xfa | 0xfb => Ok((vec![Format::ConstantPoolIndex16(Type::Method, extract_from_le!(u16, bytes, 2)), Format::ConstantPoolIndex16(Type::Prototype, 6)], 8)),
-                0x15 | 0x19 => Ok((vec![Format::ImmediateSignedHat(extract_from_le!(i16, bytes, 2))], 4)),
-                0x14 | 0x17 => Ok((vec![Format::ImmediateSigned32(extract_from_le!(u32, bytes, 2))], 6)),
-                0x18 => Ok((vec![Format::ImmediateSigned64(extract_from_le!(u64, bytes, 2))], 10)),
-                0x12 => Ok((vec![Format::ImmediateSignedNibble(bytes[1] as i8)], 2)),
-                0x13 | 0x16 | 0xd0..=0xd7 => Ok((vec![Format::ImmediateSignedShort(extract_from_le!(i16, bytes, 2))], 4)),
-                0x28 => Ok((vec![Format::BranchTarget8(bytes[1] as i8)], 2)),
-                0x29 | 0x32..=0x3d => Ok((vec![Format::BranchTarget16(extract_from_le!(i16, bytes, 2))], 4)),
-                0x26 | 0x2a..=0x2c => Ok((vec![Format::BranchTarget32(extract_from_le!(i32, bytes, 2))], 6)),
+                0x1 | 0x4 | 0x7 | 0xa..=0x11 | 0x1d | 0x1e | 0x21 | 0x27 | 0x7b..=0x8f | 0xb0..=0xcf => Ok((vec![Argument::PackedRegister(immediate_args)], 1)),
+                0x2 | 0x5 | 0x8 =>  Ok((vec![Argument::PackedRegister(immediate_args), Argument::WideRegister(raw_bytecode[1])], 2)),
+                0x2d..=0x31 | 0x44..=0x51 | 0x90..=0xaf => {
+                    let splitted = split_word!(raw_bytecode[1]);
+                    Ok((
+                        vec![
+                            Argument::PackedRegister(immediate_args), 
+                            Argument::PackedRegister(splitted.0), 
+                            Argument::PackedRegister(splitted.1),
+                        ], 
+                        2
+                    ))
+                },
+                0x3 | 0x6 | 0x9 => Ok(
+                    (vec![
+                        Argument::WideRegister(raw_bytecode[1]),
+                        Argument::WideRegister(raw_bytecode[2])
+                    ], 3)
+                ),
+                0xd8..=0xe2 => {
+                    let splitted = split_word!(raw_bytecode[1]);
+                    Ok(
+                        (vec![
+                            Argument::PackedRegister(immediate_args), 
+                            Argument::PackedRegister(splitted.0), 
+                            Argument::ImmediateSignedByte(splitted.1)
+                        ], 2)
+                    )
+                },
+                0x1a => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::String, raw_bytecode[1])], 2)),
+                0x1c | 0x1f | 0x20 | 0x22 | 0x23 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::Type, raw_bytecode[1])], 2)),
+                0x52..=0x5f => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::InstanceField, raw_bytecode[1])], 2)),
+                0x60..=0x6d => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::StaticField, raw_bytecode[1])], 2)),
+                0xfe => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::MethodHandle, raw_bytecode[1])], 2)),
+                0xff => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex16(Type::MethodPrototype, raw_bytecode[1])], 2)),
+                0x1b => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ConstantPoolIndex32(Type::String, concat_words!(raw_bytecode[1], raw_bytecode[2]))], 3)),
+                0x24 | 0x25 => Ok((vec![], 3)),  // TODO: Parse arguments here
+                0x6e..=0x72 | 0x74..=0x78 => Ok((vec![], 3)),  // TODO: Parse arguments here
+                0xfc | 0xfd => Ok((vec![Argument::ConstantPoolIndex16(Type::CallSite, raw_bytecode[2])], 3)),  // TODO: Parse registers
+                0xfa | 0xfb => Ok((vec![Argument::ConstantPoolIndex16(Type::Method, raw_bytecode[2]), Argument::ConstantPoolIndex16(Type::Prototype, raw_bytecode[3])], 4)),  // TODO: Parse registers
+                0x15 | 0x19 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ImmediateSignedHat(raw_bytecode[1] as i16)], 2)),
+                0x14 | 0x17 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ImmediateSigned32(concat_words!(raw_bytecode[1], raw_bytecode[2]))], 3)),
+                0x18 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ImmediateSigned64(concat_words!(raw_bytecode[1], raw_bytecode[2], raw_bytecode[3], raw_bytecode[4]))], 5)),
+                0x12 => Ok((vec![Argument::PackedRegister(immediate_args >> 4), Argument::ImmediateSignedNibble((immediate_args & 0xf) as i8)], 1)),
+                0x13 | 0x16 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::ImmediateSignedShort(raw_bytecode[1] as i16)], 2)), 
+                0xd0..=0xd7 => Ok((vec![Argument::PackedRegister(immediate_args >> 4), Argument::PackedRegister(immediate_args & 0xf), Argument::ImmediateSignedShort(raw_bytecode[1] as i16)], 2)),
+                0x28 => Ok((vec![Argument::BranchTarget8(immediate_args as i8)], 1)),
+                0x29 => Ok((vec![Argument::BranchTarget16(raw_bytecode[1] as i16)], 2)),
+                0x32..=0x3d => Ok((vec![Argument::PackedRegister(immediate_args >> 4), Argument::PackedRegister(immediate_args & 0xf), Argument::BranchTarget16(raw_bytecode[1] as i16)], 2)),
+                0x26 => Ok((vec![Argument::PackedRegister(immediate_args), Argument::BranchTarget32(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32)], 3)),
+                0x2a..=0x2c => Ok((vec![Argument::BranchTarget32(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32)], 3)),
                 0x3e..=0x43 | 0x73 | 0x79..=0x7a | 0xe3..=0xf9 => Err("Unimplemented"),
             } {
                 return Ok(Some(Instruction { opcode, arguments, length, offset }));
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 mod opcode;
 mod instruction;
 
-use dex::DexReader;
+use dex::{DexReader, code::CodeItem, ushort};
 use instruction::Instruction;
+
 
 fn main() {
     let dex = DexReader::from_file("resources/classes.dex").unwrap();
-    let mut counter = 0;
     for class in dex.classes() {
         if let Ok(class) = class {
             for method in class.methods() {
                 println!("{:?}.{}", class.jtype().type_descriptor(), method.name());
                 if let Some(code) = method.code() {
-                    let bytecode_sequence = code.insns().iter().map(|x| x.to_le_bytes()).flatten().collect::<Vec<u8>>();
                     let mut offset = 0;
                     let mut instructions = vec![];
-                    while offset < bytecode_sequence.len() {
-                        if let Some(inst) = Instruction::try_from_bytes(&bytecode_sequence, offset).unwrap() {
-                            println!("{:?}", inst);
+                    let raw_bytecode = code.insns();
+                    while offset < raw_bytecode.len() {
+                        if let Some(inst) = Instruction::try_from_raw_bytecode(&raw_bytecode, offset).unwrap() {
+                            println!("    {:?}", inst);
                             offset += inst.length() as usize;
                             instructions.push(inst);
                         } else {
@@ -25,7 +25,6 @@ fn main() {
                         }
                     }
                 }
-                counter += 1;
             }
         }
     }
