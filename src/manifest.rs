89dc820f2dc99be7e42554734f77c7f8f2349069
Vlad-Sebastian Cretu <vcretu@bitdefender.com>
2024-05-12 21:06:36 +0300

    Vec -> HashSet

--- src/manifest.rs
+++ src/manifest.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use axmldecoder::{Node, ParseError, XmlDocument};
 use log::warn;
 use serde::Serialize;
@@ -7,7 +8,7 @@ pub struct Manifest {
     #[serde(rename = "pkg")]
     pub package: Option<String>,
     #[serde(rename = "prm")]
-    pub permissions: Vec<String>,
+    pub permissions: HashSet<String>,
     #[serde(rename = "act")]
     pub activities: Vec<String>,
     #[serde(rename = "svc")]
@@ -37,7 +38,10 @@ pub fn parse(buf: &[u8]) -> Result<Option<Manifest>, ParseError> {
     match root {
         Some(Node::Element(root)) => {
             let package = root.attributes.get("package").map(|s| s.to_string());
-            let mut manifest = Manifest { package, ..Default::default() };
+            let mut manifest = Manifest {
+                package,
+                ..Default::default()
+            };
             for node in root.children {
                 match node {
                     Node::Element(mut element) => {
@@ -45,7 +49,7 @@ pub fn parse(buf: &[u8]) -> Result<Option<Manifest>, ParseError> {
                         if tag == "uses-permission" {
                             if let Some(name) = element.attributes.remove("android:name") {
                                 if let Some(perm) = name.strip_prefix("android.permission.") {
-                                    manifest.permissions.push(perm.to_string());
+                                    manifest.permissions.insert(perm.to_string());
                                 }
                             }
                         } else if tag == "application" {
@@ -54,46 +58,77 @@ pub fn parse(buf: &[u8]) -> Result<Option<Manifest>, ParseError> {
                                     Node::Element(mut element) => {
                                         let tag = element.get_tag();
                                         match tag {
-                                            "activity" => push_component!(element, &manifest, manifest.activities),
-                                            "service" => push_component!(element, &manifest, manifest.services),
-                                            "receiver" => push_component!(element, &manifest, manifest.receivers),
-                                            "provider" => push_component!(element, &manifest, manifest.providers),
-                                            _ => ()
+                                            "activity" => push_component!(
+                                                element,
+                                                &manifest,
+                                                manifest.activities
+                                            ),
+                                            "service" => push_component!(
+                                                element,
+                                                &manifest,
+                                                manifest.services
+                                            ),
+                                            "receiver" => push_component!(
+                                                element,
+                                                &manifest,
+                                                manifest.receivers
+                                            ),
+                                            "provider" => push_component!(
+                                                element,
+                                                &manifest,
+                                                manifest.providers
+                                            ),
+                                            _ => (),
                                         }
-                                    },
-                                    _ => ()
+                                    }
+                                    _ => (),
                                 }
                             }
                         }
-                    },
-                    _ => ()
+                    }
+                    _ => (),
                 }
             }
             Ok(Some(manifest))
-        },
+        }
         Some(other) => {
             warn!("Unexpected root node: {other:?}");
             Ok(None)
-        },
+        }
         None => Ok(None),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::Read};
     use super::parse;
+    use std::{fs::File, io::Read};
 
     #[test]
     fn test_parse() {
         let mut buf = Vec::new();
-        File::open("tests/manifest/AndroidManifest.xml").unwrap().read_to_end(&mut buf).unwrap();
+        File::open("tests/manifest/AndroidManifest.xml")
+            .unwrap()
+            .read_to_end(&mut buf)
+            .unwrap();
         let manifest = parse(&buf).unwrap().unwrap();
         assert_eq!(manifest.package, Some("com.test.dexompiler".to_string()));
-        assert_eq!(manifest.permissions, vec!["INTERNET".to_string(), "FOREGROUND_SERVICE".to_string()]);
-        assert_eq!(manifest.activities, vec!["com.test.TestActivity".to_string()]);
+        assert_eq!(
+            manifest.permissions,
+            vec!["INTERNET".to_string(), "FOREGROUND_SERVICE".to_string()].into_iter().collect()
+        );
+        assert_eq!(
+            manifest.activities,
+            vec!["com.test.TestActivity".to_string()]
+        );
         assert_eq!(manifest.services, vec!["com.test.TestService".to_string()]);
-        assert_eq!(manifest.receivers, vec!["com.test.TestReceiver".to_string()]);
-        assert_eq!(manifest.providers, vec!["com.test.TestProvider".to_string()]);
+        assert_eq!(
+            manifest.receivers,
+            vec!["com.test.TestReceiver".to_string()]
+        );
+        assert_eq!(
+            manifest.providers,
+            vec!["com.test.TestProvider".to_string()]
+        );
     }
 }
